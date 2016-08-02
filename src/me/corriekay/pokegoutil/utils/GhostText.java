81fd78dc079ecd68ecb5382b2633ad22b4da52c3
JFCaron <JFCaron@WHITEBEAST>
2016-08-02 13:25:55 -0400

    Text format

--- src/me/corriekay/pokegoutil/utils/GhostText.java
+++ src/me/corriekay/pokegoutil/utils/GhostText.java
@@ -1,4 +1,5 @@
 package me.corriekay.pokegoutil.utils;
+
 import java.awt.Color;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
@@ -9,122 +10,99 @@ import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 import javax.swing.text.JTextComponent;
 
-public class GhostText implements FocusListener, DocumentListener, PropertyChangeListener
-{
-    private final JTextComponent textComp;
-    private boolean isEmpty;
-    private Color ghostColor;
-    private Color foregroundColor;
-    private final String ghostText;
+public class GhostText implements FocusListener, DocumentListener, PropertyChangeListener {
+	private final JTextComponent textComp;
+	private boolean isEmpty;
+	private Color ghostColor;
+	private Color foregroundColor;
+	private final String ghostText;
 
-    public GhostText(final JTextComponent textComp, String ghostText)
-    {
-        super();
-        this.textComp = textComp;
-        this.ghostText = ghostText;
-        this.ghostColor = Color.LIGHT_GRAY;
-        textComp.addFocusListener(this);
-        registerListeners();
-        updateState();
-        if (!this.textComp.hasFocus())
-        {
-            focusLost(null);
-        }
-    }
+	public GhostText(final JTextComponent textComp, String ghostText) {
+		super();
+		this.textComp = textComp;
+		this.ghostText = ghostText;
+		this.ghostColor = Color.LIGHT_GRAY;
+		textComp.addFocusListener(this);
+		registerListeners();
+		updateState();
+		if (!this.textComp.hasFocus()) {
+			focusLost(null);
+		}
+	}
 
-    public void delete()
-    {
-        unregisterListeners();
-        textComp.removeFocusListener(this);
-    }
+	public void delete() {
+		unregisterListeners();
+		textComp.removeFocusListener(this);
+	}
 
-    private void registerListeners()
-    {
-        textComp.getDocument().addDocumentListener(this);
-        textComp.addPropertyChangeListener("foreground", this);
-    }
+	private void registerListeners() {
+		textComp.getDocument().addDocumentListener(this);
+		textComp.addPropertyChangeListener("foreground", this);
+	}
 
-    private void unregisterListeners()
-    {
-        textComp.getDocument().removeDocumentListener(this);
-        textComp.removePropertyChangeListener("foreground", this);
-    }
+	private void unregisterListeners() {
+		textComp.getDocument().removeDocumentListener(this);
+		textComp.removePropertyChangeListener("foreground", this);
+	}
 
-    public Color getGhostColor()
-    {
-        return ghostColor;
-    }
+	public Color getGhostColor() {
+		return ghostColor;
+	}
 
-    public void setGhostColor(Color ghostColor)
-    {
-        this.ghostColor = ghostColor;
-    }
+	public void setGhostColor(Color ghostColor) {
+		this.ghostColor = ghostColor;
+	}
 
-    private void updateState()
-    {
-        isEmpty = textComp.getText().length() == 0;
-        foregroundColor = textComp.getForeground();
-    }
+	private void updateState() {
+		isEmpty = textComp.getText().length() == 0;
+		foregroundColor = textComp.getForeground();
+	}
 
-    @Override
-    public void focusGained(FocusEvent e)
-    {
-        if (isEmpty)
-        {
-            unregisterListeners();
-            try
-            {
-                textComp.setText("");
-                textComp.setForeground(foregroundColor);
-            }
-            finally
-            {
-                registerListeners();
-            }
-        }
+	@Override
+	public void focusGained(FocusEvent e) {
+		if (isEmpty) {
+			unregisterListeners();
+			try {
+				textComp.setText("");
+				textComp.setForeground(foregroundColor);
+			} finally {
+				registerListeners();
+			}
+		}
 
-    }
+	}
 
-    @Override
-    public void focusLost(FocusEvent e)
-    {
-        if (isEmpty)
-        {
-            unregisterListeners();
-            try
-            {
-                textComp.setText(ghostText);
-                textComp.setForeground(ghostColor);
-            }
-            finally
-            {
-                registerListeners();
-            }
-        }
-    }
+	@Override
+	public void focusLost(FocusEvent e) {
+		if (isEmpty) {
+			unregisterListeners();
+			try {
+				textComp.setText(ghostText);
+				textComp.setForeground(ghostColor);
+			} finally {
+				registerListeners();
+			}
+		}
+	}
 
-    @Override
-    public void propertyChange(PropertyChangeEvent evt)
-    {
-        updateState();
-    }
+	@Override
+	public void propertyChange(PropertyChangeEvent evt) {
+		updateState();
+	}
 
-    @Override
-    public void changedUpdate(DocumentEvent e)
-    {
-        updateState();
-    }
+	@Override
+	public void changedUpdate(DocumentEvent e) {
+		updateState();
+	}
 
-    @Override
-    public void insertUpdate(DocumentEvent e)
-    {
-        updateState();
-    }
+	@Override
+	public void insertUpdate(DocumentEvent e) {
+		updateState();
+	}
 
-    @Override
-    public void removeUpdate(DocumentEvent e)
-    {
-        updateState();
-    }
+	@Override
+	public void removeUpdate(DocumentEvent e) {
+		updateState();
+	}
 
 }
