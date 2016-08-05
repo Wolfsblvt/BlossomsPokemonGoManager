60d5aa662eba1cfb97d253bcc8b7afaabcacde30
JFCaron <JFCaron@WHITEBEAST>
2016-08-05 12:53:19 -0400

    Added base singleton structure for controllers

--- src/me/corriekay/pokegoutil/controllers/AccountController.java
+++ src/me/corriekay/pokegoutil/controllers/AccountController.java
@@ -1,5 +1,32 @@
 package me.corriekay.pokegoutil.controllers;
 
-public class AccountController {
-	//this controller does the login/log off, and different account information (aka player data)
+import com.pokegoapi.api.PokemonGo;
+
+/*this controller does the login/log off, and different account information (aka player data)
+ * 
+ */
+public final class AccountController {
+	
+	private static final AccountController S_INSTANCE = new AccountController();
+	private static boolean sIsInit = false;
+	
+	private PokemonGo go;
+		
+	private AccountController(){
+		
+	}
+	
+	public static AccountController getInstance(){
+		return S_INSTANCE;
+	}
+	
+	public static void initialize(PokemonGo go){
+		if(sIsInit)
+			return;
+		
+		S_INSTANCE.go = go;
+		
+		sIsInit = true;
+	}
+	
 }
--- src/me/corriekay/pokegoutil/controllers/PokemonBagController.java
+++ src/me/corriekay/pokegoutil/controllers/PokemonBagController.java
@@ -1,5 +1,31 @@
 package me.corriekay.pokegoutil.controllers;
 
+import com.pokegoapi.api.PokemonGo;
+
+/*
+ * This controller takes care of handling pokémon
+ */
 public class PokemonBagController {
-	//This controller takes care of handling pokemons
+	
+	private static final PokemonBagController S_INSTANCE = new PokemonBagController();
+	private static boolean sIsInit = false;
+	
+	private PokemonGo go;
+		
+	private PokemonBagController(){
+		
+	}
+	
+	public static PokemonBagController getInstance(){
+		return S_INSTANCE;
+	}
+	
+	public static void initialize(PokemonGo go){
+		if(sIsInit)
+			return;
+		
+		S_INSTANCE.go = go;
+		
+		sIsInit = true;
+	}
 }
