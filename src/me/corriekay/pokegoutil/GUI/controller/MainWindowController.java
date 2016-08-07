605cbbb60bccee3b4f9ed444eb1ad207a68732da
JFCaron <JFCaron@WHITEBEAST>
2016-08-07 15:40:29 -0400

    New GUI structure
    You can still use the old GUI in the dialog box
    
    (new GUI is just a template for now, will fill in the next commit, and
    include login, etc)

---
+++ src/me/corriekay/pokegoutil/GUI/controller/MainWindowController.java
@@ -0,0 +1,47 @@
+package me.corriekay.pokegoutil.GUI.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuItem;
+
+public class MainWindowController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private MenuItem settingsMenuItem;
+
+    @FXML
+    private MenuItem logOffMenuItem;
+
+    @FXML
+    private MenuItem quitMenuItem;
+    
+    @FXML
+    void onAboutClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onLogOffClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onQuitClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onSettingsClicked(ActionEvent event) {
+
+    }
+
+}
