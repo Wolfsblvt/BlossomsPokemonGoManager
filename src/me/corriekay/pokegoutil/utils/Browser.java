c4d1d03d9e1c1c6dbe2ce28ed5b52376ee16100c
Wolfsblvt <clemenshusung@gmx.de>
2016-08-09 01:51:01 +0200

    docblock!

--- src/me/corriekay/pokegoutil/utils/Browser.java
+++ src/me/corriekay/pokegoutil/utils/Browser.java
@@ -8,6 +8,12 @@ import java.net.URISyntaxException;
 
 public class Browser {
 
+    /***
+     * Opens given URL in users default browser of his operating system.
+     * Should work for all operating systems, with fallback.
+     * @param url The URL to open in browser
+     * @return If opening was successful
+     */
     public static boolean openUrl(String url) {
         // We start to check if Desktop API is supported. In that case everything is easy.
         if (Desktop.isDesktopSupported()) {
