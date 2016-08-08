76134ffdebef328db26541c208ec3fc7b1f22728
JFCaron <jfrancois.c@gmail.com>
2016-08-08 16:23:13 -0400

    Structure for MainWindow and adds a checkbox to the login window

--- src/me/corriekay/pokegoutil/GUI/controller/LoginController.java
+++ src/me/corriekay/pokegoutil/GUI/controller/LoginController.java
@@ -14,6 +14,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.StackPane;
