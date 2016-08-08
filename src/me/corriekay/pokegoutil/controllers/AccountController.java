8b47361b9abfe304d5eb49cff7a766a47208480d
Wolfsblvt <clemenshusung@gmx.de>
2016-08-08 16:14:28 +0200

    CrossOS solution for URL open on Google OAuth token

--- src/me/corriekay/pokegoutil/controllers/AccountController.java
+++ src/me/corriekay/pokegoutil/controllers/AccountController.java
@@ -19,6 +19,7 @@ import com.pokegoapi.auth.CredentialProvider;
 import com.pokegoapi.auth.GoogleUserCredentialProvider;
 import com.pokegoapi.auth.PtcCredentialProvider;
 
+import me.corriekay.pokegoutil.utils.Browser;
 import me.corriekay.pokegoutil.utils.Config;
 import me.corriekay.pokegoutil.utils.Console;
 import me.corriekay.pokegoutil.windows.PokemonGoMainWindow;
@@ -115,11 +116,12 @@ public final class AccountController {
 					//We need to get the auth code, as we do not have it yet.
 					UIManager.put("OptionPane.okButtonText", "Ok");
 					JOptionPane.showMessageDialog(null, "You will need to provide a google authentication key to log in. Press OK to continue.", "Google Auth", JOptionPane.PLAIN_MESSAGE);
-					//We're gonna try to load the page using the users browser 
-					if(Desktop.isDesktopSupported()) {
-						JOptionPane.showMessageDialog(null, "A webpage should open up, please allow the permissions, and then copy the code into your clipboard. Press OK to continue", "Google Auth", JOptionPane.PLAIN_MESSAGE);
-						Desktop.getDesktop().browse(new URI(GoogleUserCredentialProvider.LOGIN_URL));
-					} else {
+
+					//We're gonna try to load the page using the users browser
+					JOptionPane.showMessageDialog(null, "A webpage should open up, please allow the permissions, and then copy the code into your clipboard. Press OK to continue", "Google Auth", JOptionPane.PLAIN_MESSAGE);
+					boolean success = Browser.openUrl(GoogleUserCredentialProvider.LOGIN_URL);
+					if(!success) {
+						// Okay, couldn't open it. We use the manual copy window
 						UIManager.put("OptionPane.cancelButtonText", "Copy To Clipboard");
 						if(JOptionPane.showConfirmDialog(null, "Please copy this link and paste it into a browser.\nThen, allow the permissions, and copy the code into your clipboard.", "Google Auth", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE) == JOptionPane.CANCEL_OPTION){
 							StringSelection ss = new StringSelection(GoogleUserCredentialProvider.LOGIN_URL);
---
+++ src/me/corriekay/pokegoutil/utils/Browser.java
@@ -0,0 +1,90 @@
+package me.corriekay.pokegoutil.utils;
+
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class Browser {
+
+    public static boolean openUrl(String url) {
+        // We start to check if Desktop API is supported. In that case everything is easy.
+        if (Desktop.isDesktopSupported()) {
+            Desktop desktop = Desktop.getDesktop();
+            try {
+                // Okay, that was easy
+                desktop.browse(new URI(url));
+                return true;
+            } catch (IOException | URISyntaxException e) {
+                // Something went wrong with desktop, we try running the crossPlatformSolution
+                e.printStackTrace();
+                return tryCrossPlatformOpenUrl(url);
+            }
+        } else {
+
+            return false;
+        }
+    }
+
+    private static boolean tryCrossPlatformOpenUrl(String url) {
+        // We need the OS
+        String os = System.getProperty("os.name").toLowerCase();
+
+        // Check for windows first
+        if (os.contains("win")) {
+            try {
+                Runtime rt = Runtime.getRuntime();
+                rt.exec("rundll32 url.dll,FileProtocolHandler " + url);
+                return true;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // Now lets try Mac systems
+        if (os.contains("mac")) {
+            try {
+                Runtime rt = Runtime.getRuntime();
+                rt.exec("open" + url);
+                return true;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // That's like the best try for Linux systems
+        if (os.contains("nix") || os.contains("nux")) {
+            try {
+                Runtime runtime = Runtime.getRuntime();
+                runtime.exec("xdg-open " + url);
+                return true;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // Last chance for Linux now
+        if (os.contains("nix") || os.contains("nux")) {
+            Runtime rt = Runtime.getRuntime();
+            String[] browsers = {"epiphany", "firefox", "mozilla", "konqueror",
+                    "netscape", "opera", "links", "lynx"};
+
+            StringBuilder cmd = new StringBuilder();
+            for (int i = 0; i < browsers.length; i++)
+                cmd.append(i == 0 ? "" : " || ").append(browsers[i]).append(" \"").append(url).append("\" ");
+
+            try {
+                rt.exec(new String[]{"sh", "-c", cmd.toString()});
+                return true;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // Dear lord, everything has failed. No chance here anymore. I don't know what else I can do.
+        // Mum, I have failed.
+        System.out.println("Found no chance to open Browser URL. Terminate now.");
+        return false;
+    }
+}
