562b0fa40fe33b244b67a12777974a44b50f5052
Wolfsblvt <clemenshusung@gmx.de>
2016-08-10 22:49:00 +0200

    Second: Formatting

--- src/me/corriekay/pokegoutil/GUI/controller/ChooseGuiWindowController.java
+++ src/me/corriekay/pokegoutil/GUI/controller/ChooseGuiWindowController.java
@@ -1,4 +1,5 @@
 package me.corriekay.pokegoutil.GUI.controller;
+
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -19,75 +20,75 @@ import me.corriekay.pokegoutil.controllers.AccountController;
 import me.corriekay.pokegoutil.utils.Console;
 import me.corriekay.pokegoutil.utils.Utilities;
 
-public class ChooseGuiWindowController extends Pane{
-	
+public class ChooseGuiWindowController extends Pane {
+
     @FXML
     private ResourceBundle resources;
-    
+
     @FXML
     private URL location;
-    
+
     @FXML
     private URL icon;
 
-	@FXML
+    @FXML
     private Button oldGuiBtn;
 
     @FXML
     private Button newGuiBtn;
-	
-	public URL getIcon(){
-		return icon;
-	}
-	
-	public URL getLocation(){
-		return location;
-	}
-    
-    public ChooseGuiWindowController(){
+
+    public URL getIcon() {
+        return icon;
     }
-    
+
+    public URL getLocation() {
+        return location;
+    }
+
+    public ChooseGuiWindowController() {
+    }
+
     @FXML
     void initialize() {
     }
 
     @FXML
-    void onNewGuiBtnClicked(ActionEvent event){
-    	newGuiBtn.getScene().getWindow().hide();	
-    	ClassLoader classLoader = getClass().getClassLoader();
-    	Parent login = new LoginController();		
-		try {
-			login = (Parent) FXMLLoader.load(classLoader.getResource("layout/Login.fxml"));
-		} catch (IOException e) {
-			System.err.println("Problem loading .fxml file: " + e.toString());
-			return;
-		}
-    	
-		Stage loginWindow = new Stage();		
-		URL icon = classLoader.getResource("icon/PokeBall-icon.png");
-		loginWindow.getIcons().add(new Image(icon.toExternalForm()));
-		loginWindow.setTitle("Login");
-		loginWindow.initStyle(StageStyle.UTILITY);
-		loginWindow.setResizable(false);		
-		loginWindow.setScene(new Scene(login));    	  
-		loginWindow.show();
+    void onNewGuiBtnClicked(ActionEvent event) {
+        newGuiBtn.getScene().getWindow().hide();
+        ClassLoader classLoader = getClass().getClassLoader();
+        Parent login = new LoginController();
+        try {
+            login = (Parent) FXMLLoader.load(classLoader.getResource("layout/Login.fxml"));
+        } catch (IOException e) {
+            System.err.println("Problem loading .fxml file: " + e.toString());
+            return;
+        }
+
+        Stage loginWindow = new Stage();
+        URL icon = classLoader.getResource("icon/PokeBall-icon.png");
+        loginWindow.getIcons().add(new Image(icon.toExternalForm()));
+        loginWindow.setTitle("Login");
+        loginWindow.initStyle(StageStyle.UTILITY);
+        loginWindow.setResizable(false);
+        loginWindow.setScene(new Scene(login));
+        loginWindow.show();
     }
 
     @FXML
     void onOldGuiBtnClicked(ActionEvent event) {
-    	oldGuiBtn.getScene().getWindow().hide();
-    	SwingUtilities.invokeLater(new Runnable() {
+        oldGuiBtn.getScene().getWindow().hide();
+        SwingUtilities.invokeLater(new Runnable() {
 
             private Console console;
 
-			@Override
-            public void run() {            	
-        		Utilities.setNativeLookAndFeel();
-        		console = new Console("Console", 0, 0, true);
-        		console.setVisible(false);
-        		AccountController.initialize(console);
-				AccountController.logOn();				
+            @Override
+            public void run() {
+                Utilities.setNativeLookAndFeel();
+                console = new Console("Console", 0, 0, true);
+                console.setVisible(false);
+                AccountController.initialize(console);
+                AccountController.logOn();
             }
-        });    	    	
+        });
     }
 }
