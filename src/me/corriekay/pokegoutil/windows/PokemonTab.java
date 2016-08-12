a4dd191c3d9bfeacd8bba12f5f1c130fe400a28e
Ritsu <ritsu@qiguang.net>
2016-08-12 02:26:47 -0400

    Added font size dropdown

--- src/me/corriekay/pokegoutil/windows/PokemonTab.java
+++ src/me/corriekay/pokegoutil/windows/PokemonTab.java
@@ -29,8 +29,6 @@ import javax.swing.table.AbstractTableModel;
 import javax.swing.table.TableModel;
 import javax.swing.table.TableRowSorter;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.*;
