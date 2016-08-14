06af29210a3df8294c3c7729503b2526510a158e
Cryptic <Cryptically@hotmail.com>
2016-08-14 17:02:35 +0800

    Changing to use NewConfig - Phase 3

--- src/me/corriekay/pokegoutil/utils/ConfigKey.java
+++ src/me/corriekay/pokegoutil/utils/ConfigKey.java
@@ -9,6 +9,19 @@ public enum ConfigKey {
 	TRANSFER_AFTER_EVOLVE("transfer.afterEvolve", false, Boolean.class),
 	SHOW_BULK_POPUP("popup.afterBulk", true, Boolean.class),
 	
+	LANGUAGE("options.lang", "en", String.class),
+	FONT_SIZE("options.fontsize", 0, Integer.class),
+	
+	DELAY_RENAME_MIN("delay.rename.min", 1000, Integer.class),
+	DELAY_RENAME_MAX("delay.rename.max", 5000, Integer.class),
+	DELAY_TRANSFER_MIN("delay.transfer.min", 1000, Integer.class),
+	DELAY_TRANSFER_MAX("delay.transfer.max", 5000, Integer.class),
+	DELAY_EVOLVE_MIN("delay.evolve.min", 3000, Integer.class),
+	DELAY_EVOLVE_MAX("delay.evolve.max", 12000, Integer.class),
+	DELAY_POWERUP_MIN("delay.powerUp.min", 1000, Integer.class),
+	DELAY_POWERUP_MAX("delay.powerUp.max", 5000, Integer.class),
+	DELAY_FAVORITE_MIN("delay.favorite.min", 1000, Integer.class),
+	DELAY_FAVORITE_MAX("delay.favorite.max", 3000, Integer.class),
 	;
 	
 	public final String keyName;
