45f4507c0c25da5dc25833a348719bff1b8b1b15
Ritsu <ritsu@qiguang.net>
2016-08-23 03:06:05 -0400

    checkstyles

--- src/me/corriekay/pokegoutil/windows/PokemonTab.java
+++ src/me/corriekay/pokegoutil/windows/PokemonTab.java
@@ -386,8 +386,7 @@ public class PokemonTab extends JPanel {
                                 if (newPoke.isFavorite()) {
                                     System.out.println("Skipping \"Transfer After Evolve\" for " + StringUtils.capitalize(newPoke.getPokemonId().toString().toLowerCase()) + " because favorite.");
                                     System.out.println("Stat changes: (Candies: " + newCandies + "[" + candies + "-" + candiesToEvolve + "], CP: " + newCp + "[+" + (newCp - cp) + "], HP: " + newHp + "[+" + (newHp - hp) + "])");
-                                }
-                                else {
+                                } else {
                                     ReleasePokemonResponseOuterClass.ReleasePokemonResponse.Result result = newPoke.transferPokemon();
                                     System.out.println("Transferring " + StringUtils.capitalize(newPoke.getPokemonId().toString().toLowerCase()) + ", Result: " + result);
                                     System.out.println("Stat changes: (Candies: " + newCandies + "[" + candies + "-" + candiesToEvolve + "]");
