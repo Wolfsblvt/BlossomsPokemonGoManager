0e414964829fe8b769a3474e6bd63fe0491bed41
Cryptic <Cryptically@hotmail.com>
2016-09-03 16:21:19 +0800

    Prep for operation handling

--- src/me/corriekay/pokegoutil/DATA/models/BPMOperationResult.java
+++ src/me/corriekay/pokegoutil/DATA/models/BPMOperationResult.java
@@ -1,23 +1,49 @@
 package me.corriekay.pokegoutil.DATA.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import me.corriekay.pokegoutil.DATA.enums.OperationError;
+import me.corriekay.pokegoutil.GUI.enums.OperationID;
 
 public class BPMOperationResult extends BPMResult {
-    
+
+    private List<String> successMessageList = new ArrayList<String>();
     private OperationError operationError;
+    private OperationID nextOperation;
+
     public BPMOperationResult() {
         super();
     }
-
-    public BPMOperationResult(String errorMessage, OperationError operationError){
+    
+    public BPMOperationResult(String errorMessage, OperationError operationError) {
         super(errorMessage);
         this.operationError = operationError;
     }
-
+    
+    public void addSuccessMessage(String successMessage) {
+        successMessageList.add(successMessage);
+    }
+    
+    public OperationID getNextOperation() {
+        return nextOperation;
+    }
     public OperationError getOperationError() {
         return operationError;
     }
-    
+
+    public List<String> getSuccessMessageList() {
+        return successMessageList;
+    }
+
+    public boolean hasNextOperation(){
+        return nextOperation != null;
+    }
+
+    public void setNextOperation(OperationID nextOperation) {
+        this.nextOperation = nextOperation;
+    }
+
     public void setOperationError(OperationError operationError) {
         this.operationError = operationError;
     }
