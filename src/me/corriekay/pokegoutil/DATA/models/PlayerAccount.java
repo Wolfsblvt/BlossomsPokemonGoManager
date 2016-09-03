958113ded971cf32d97bed38c3899229a78b73bf
Cryptic <Cryptically@hotmail.com>
2016-09-04 01:10:40 +0800

    Javadoc for PlayerAccount

--- src/me/corriekay/pokegoutil/DATA/models/PlayerAccount.java
+++ src/me/corriekay/pokegoutil/DATA/models/PlayerAccount.java
@@ -10,10 +10,23 @@ public class PlayerAccount {
 
     private final PlayerProfile playerProfile;
 
+    /**
+     * Instantiate PlayerAccount controller
+     *
+     * @param playerProfile player profile of logged on user
+     */
     public PlayerAccount(final PlayerProfile playerProfile) {
         this.playerProfile = playerProfile;
     }
 
+    /**
+     * Get number of stardust
+     * 
+     * @return number of stardust
+     * @throws InvalidCurrencyException invalid currency
+     * @throws LoginFailedException login failed
+     * @throws RemoteServerException server error
+     */
     public int getStardust() throws InvalidCurrencyException, LoginFailedException, RemoteServerException {
         return playerProfile.getCurrency(Currency.STARDUST);
     }
