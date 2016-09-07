c598b9862f9ac1b50366d4ac9b672b9d15e3cf51
Wolfsblvt <clemenshusung@gmx.de>
2016-09-08 00:28:30 +0200

    Missing file

--- src/me/corriekay/pokegoutil/GUI/enums/OperationID.java
+++ src/me/corriekay/pokegoutil/GUI/enums/OperationID.java
@@ -3,35 +3,44 @@ package me.corriekay.pokegoutil.GUI.enums;
 import java.util.HashMap;
 import java.util.Map;
 
-public enum OperationID {
+public enum OperationId {
     RENAME("Rename","Renaming","Renamed"),
     TRANSFER("Transfer","Transfering","Transfered"),
     POWERUP("Power Up","Powering Up","Powered Up"),
     EVOLVE("Evolve","Evolving","Evolved"),
     FAVORITE("Favorite","Favoriting","Favorited");
 
-    private static final Map<String,OperationID> operationMap = new HashMap<String, OperationID>();
+    private static final Map<String, OperationId> operationMap = new HashMap<String, OperationId>();
 
     static {
-        for(OperationID id : OperationID.values())
+        for (final OperationId id : OperationId.values()) {
             operationMap.put(id.getActionName(), id);
+        }
     }
 
     private String actionName;
     private String actionVerbDuring;
     private String actionVerbFinished;
 
-    private OperationID(String actionName, String actionVerbDuring, String actionVerbFinished) {
+    private OperationId(final String actionName, final String actionVerbDuring, final String actionVerbFinished) {
         this.actionName = actionName;
         this.actionVerbDuring = actionVerbDuring;
         this.actionVerbFinished = actionVerbFinished;
     }
 
-    public String getActionName() { return actionName; }
-    public String getActionVerbDuring() { return actionVerbDuring; }
-    public String getActionVerbFinished() { return actionVerbFinished; }
+    public String getActionName() {
+        return actionName;
+    }
 
-    public static OperationID get(String actionName) {
+    public String getActionVerbDuring() {
+        return actionVerbDuring;
+    }
+
+    public String getActionVerbFinished() {
+        return actionVerbFinished;
+    }
+
+    public static OperationId get(final String actionName) {
         return operationMap.get(actionName);
     }
 }
