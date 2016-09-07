a4c3be505cfe17b0b5cdbee1e6099f54ee828d7f
Wolfsblvt <clemenshusung@gmx.de>
2016-09-07 23:28:15 +0200

    Fix a bit more Codacy Errors

--- src/me/corriekay/pokegoutil/utils/windows/DefaultCellRenderer.java
+++ src/me/corriekay/pokegoutil/utils/windows/DefaultCellRenderer.java
@@ -1,40 +1,49 @@
 package me.corriekay.pokegoutil.utils.windows;
 
 import java.awt.Component;
-import java.text.NumberFormat;
 
 import javax.swing.JLabel;
 import javax.swing.JTable;
 import javax.swing.table.TableCellRenderer;
 
-import me.corriekay.pokegoutil.utils.Utilities;
-
 /**
- * Provide custom formatting for the moveset ranking columns while allowing sorting on original values
+ * Provide custom formatting for the moveset ranking columns while allowing sorting on original values.
  */
 
 @SuppressWarnings("serial")
 public class DefaultCellRenderer extends JLabel implements TableCellRenderer {
 
+    /**
+     * Creates an instance of the DefaultCellRenderer.
+     */
     public DefaultCellRenderer() {
+        // We don't need to do anything here. Just the default constructor.
+        super();
     }
 
     @Override
     public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected,
-                                                   boolean hasFocus, int rowIndex, int vColIndex) {
+                                                   boolean hasFocus, int rowIndex, int columnIndex) {
         setText(value.toString());
         setOpaque(true);
         setDefaultSelectionColors(table, isSelected, this);
         return this;
     }
 
-    private void setDefaultSelectionColors(JTable table, boolean isSelected, JLabel tcr) {
+    /**
+     * Set the selection colors for the cell.
+     *
+     * @param table      The table.
+     * @param isSelected If the cell is selected.
+     * @param cell       The cell.
+     */
+    private void setDefaultSelectionColors(JTable table, boolean isSelected, JLabel cell) {
         if (isSelected) {
-            tcr.setBackground(table.getSelectionBackground());
-            tcr.setForeground(table.getSelectionForeground());
+            cell.setBackground(table.getSelectionBackground());
+            cell.setForeground(table.getSelectionForeground());
         } else {
-            tcr.setBackground(table.getBackground());
-            tcr.setForeground(table.getForeground());
+            cell.setBackground(table.getBackground());
+            cell.setForeground(table.getForeground());
         }
     }
 }
