7942daf1d818dff425bbaeb72ce2cb1e9a0c6f77
Cryptic <Cryptically@hotmail.com>
2016-09-11 13:44:36 +0800

    Javadoc updates

--- src/me/corriekay/pokegoutil/gui/controller/BaseController.java
+++ src/me/corriekay/pokegoutil/gui/controller/BaseController.java
@@ -11,6 +11,11 @@ import javafx.stage.Stage;
 import me.corriekay.pokegoutil.BlossomsPoGoManager;
 import me.corriekay.pokegoutil.gui.models.GuiControllerSettings;
 
+/**
+ * The base controller used for all javaFx GUIs.
+ *
+ * @param <T> layout class to be used in the controller
+ */
 public abstract class BaseController<T extends Pane> {
     private static final ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
     private static final String iconPath = "icon/PokeBall-icon.png";
@@ -22,12 +27,13 @@ public abstract class BaseController<T extends Pane> {
         iconImage = new Image(icon.toExternalForm());
     }
 
-    private final Scene rootScene;
     private final Stage stage;
     private String title;
 
+    /**
+     * Instantiate the controller and set all the settings. Sets the primary stage to this controller.
+     */
     public BaseController() {
-
         final FXMLLoader fxmlLoader = new FXMLLoader(classLoader.getResource(getFxmlLayout()));
         fxmlLoader.setController(this);
         try {
@@ -35,7 +41,7 @@ public abstract class BaseController<T extends Pane> {
         } catch (final IOException exception) {
             throw new RuntimeException(exception);
         }
-        rootScene = new Scene(fxmlLoader.getRoot());
+        final Scene rootScene = new Scene(fxmlLoader.getRoot());
 
         final GuiControllerSettings guiControllerSettings = getGuiControllerSettings();
         stage = new Stage();
@@ -46,14 +52,34 @@ public abstract class BaseController<T extends Pane> {
         BlossomsPoGoManager.setNewPrimaryStage(stage);
     }
 
+    /**
+     * Get the path of the FxmlLayout file.
+     *
+     * @return path of the FxmlLayout file
+     */
     abstract public String getFxmlLayout();
 
+    /**
+     * Get the settings used to initialize the controller.
+     *
+     * @return settings used to initialize the controller
+     */
     abstract public GuiControllerSettings getGuiControllerSettings();
 
+    /**
+     * Get the title of the window.
+     *
+     * @return title of the window
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * Set the current title of the window.
+     *
+     * @param title title of the window
+     */
     public void setTitle(final String title) {
         this.title = title;
         stage.setTitle(title);
--- src/me/corriekay/pokegoutil/gui/models/GuiControllerSettings.java
+++ src/me/corriekay/pokegoutil/gui/models/GuiControllerSettings.java
@@ -1,26 +1,52 @@
 package me.corriekay.pokegoutil.gui.models;
 
+/**
+ * The settings used for initializing the controller.
+ */
 public class GuiControllerSettings {
     private String title;
     private boolean resizeable;
 
+    /**
+     * Instantiate a GuiControllerSettings with the default value.
+     */
     public GuiControllerSettings() {
         title = "";
         resizeable = true;
     }
 
+    /**
+     * Get the title of the controller.
+     *
+     * @return title of the controller
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * Get if the controller is resizeable.
+     *
+     * @return is controller resizeable
+     */
     public boolean isResizeable() {
         return resizeable;
     }
 
+    /**
+     * Set if the controller is resizeable
+     *
+     * @param resizeable is controller resizeable
+     */
     public void setResizeable(final boolean resizeable) {
         this.resizeable = resizeable;
     }
 
+    /**
+     * Set the title of the controller.
+     *
+     * @param title title of the controller
+     */
     public void setTitle(final String title) {
         this.title = title;
     }
