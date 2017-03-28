50d685eb51b38fa0bfc731f0f4d7f50aca34856f
kbbn2001 <wkddns1@naver.com>
2017-03-28 09:22:13 +0900

    adding testOracle package

---
+++ test/testOracle/s201224434.java
@@ -0,0 +1,14 @@
+package testOracle;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class s201224434 {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
