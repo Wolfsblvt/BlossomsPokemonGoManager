399e2be344806363089db1243c307bc322ffe673
kbbn2001 <wkddns1@naver.com>
2017-03-28 09:47:03 +0900

    create test case AutoIncrementer class

--- test/testOracle/s201224434.java
+++ test/testOracle/s201224434.java
@@ -2,13 +2,72 @@ package testOracle;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class s201224434 {
+import me.corriekay.pokegoutil.utils.*;
 
-    @Test
-    public void test() {
-        fail("Not yet implemented");
+public class s201224434 {
+    
+    
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
     }
 
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+    
+    /**
+     * Purpose : Test AutoIncrementer class defalut constructor
+     * Input : AutoIncrementer()
+     * Expected : 
+     *      return 0
+     */
+    @Test
+    public void testAutoIncrementerConstructor() {
+        AutoIncrementer autoIncrementer = new AutoIncrementer();
+        assertEquals(0, autoIncrementer.get());
+    }
+    
+    /**
+     * Purpose : Test AutoIncrementer class constructor
+     * Input : AutoIncrementer(Integer.MAX_VALUE)
+     * Expected : 
+     *      return Integer.MAX_VALUE
+     */
+    @Test
+    public void testAutoIncrementerConstructor2() {
+        AutoIncrementer autoIncrementer = new AutoIncrementer(Integer.MAX_VALUE);
+        assertEquals(Integer.MAX_VALUE, autoIncrementer.get());
+    }
+    
+    /**
+     * Purpose : Test AutoIncrementer class get method
+     * Input : AutoIncrementer() , get() , get(), get(), get()
+     * Expected : 
+     *      return 
+     */
+    @Test
+    public void testAutoIncrementerGet() {
+        AutoIncrementer autoIncrementer = new AutoIncrementer();
+        assertEquals(0, autoIncrementer.get());
+        assertEquals(1, autoIncrementer.get());
+        assertEquals(2, autoIncrementer.get());
+        assertEquals(3, autoIncrementer.get());
+    }
+    
+    
+    
 }
