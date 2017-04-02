1d2757a852796d6264d1f02224c9a26fb5402cbb
gxicxigouxa <gxicxi@gmail.com>
2017-04-02 22:14:56 +0900

    Make PokemonModelTest.java for test PokemonModel.java

---
+++ test/testOracle/PokemonModelTest.java
@@ -0,0 +1,526 @@
+package testOracle;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+/**
+ * TODO.
+ */
+public class PokemonModelTest {
+
+    @Test
+    public void testPokemonModel() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testAtkProperty() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testCandies2EvlvProperty() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testCandiesProperty() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testCaughtDateProperty() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testCpEvolvedProperty() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testCpProperty() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testDefProperty() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testDuelAbilityIvProperty() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testDuelAbilityProperty() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testDustToLevelProperty() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testEvolvableProperty() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetAtk() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetCandies() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetCandies2Evlv() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetCandyCostsForPowerup() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetCaughtDate() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetCp() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetCpEvolved() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetDef() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetDuelAbility() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetDuelAbilityIv() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetDustToLevel() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetEvolvable() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetGymDefense() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetGymDefenseIv() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetGymOffense() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetGymOffenseIv() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetHp() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetIv() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetLevel() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetMaxCp() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetMaxCpCurrent() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetMaxEvolvedCp() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetMaxEvolvedCpCurrent() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetMove1() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetMove2() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetNickname() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetNumId() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetPokeball() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetPokemon() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetSpecies() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetStam() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetStardustCostsForPowerup() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetSummary() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetType1() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetType2() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGymDefenseIvProperty() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGymDefenseProperty() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGymOffenseIvProperty() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGymOffenseProperty() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testHpProperty() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testIsFavoriteProperty() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testIsInGym() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testIsIsFavorite() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testIvProperty() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testLevelProperty() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testMaxCpCurrentProperty() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testMaxCpProperty() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testMaxEvolvedCpCurrentProperty() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testMaxEvolvedCpProperty() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testMove1Property() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testMove2Property() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testNicknameProperty() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testNumIdProperty() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testPokeballProperty() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSetAtk() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSetCandies() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSetCandies2Evlv() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSetCaughtDate() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSetCp() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSetCpEvolved() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSetDef() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSetDuelAbility() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSetDuelAbilityIv() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSetDustToLevel() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSetEvolvable() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSetGymDefense() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSetGymDefenseIv() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSetGymOffense() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSetGymOffenseIv() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSetHp() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSetIsFavorite() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSetIv() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSetLevel() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSetMaxCp() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSetMaxCpCurrent() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSetMaxEvolvedCp() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSetMaxEvolvedCpCurrent() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSetMove1() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSetMove2() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSetNickname() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSetNumId() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSetPokeball() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSetPokemon() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSetSpecies() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSetStam() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSetType1() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSetType2() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSpeciesProperty() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testStamProperty() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testType1Property() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testType2Property() {
+        fail("Not yet implemented");
+    }
+
+}
