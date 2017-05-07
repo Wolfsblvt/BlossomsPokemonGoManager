15fbc12c07997a7c8e5c4dd868bb271d457e7f59
altusdnjs@naver.com <altusdnjs@naver.com>
2017-05-08 01:30:50 +0900

    extract method, if don't have enough resources or cases print the cause.(in PowerupOperation, private needSomeToPowerUp)

--- src/me/corriekay/pokegoutil/data/models/operations/PowerupOperation.java
+++ src/me/corriekay/pokegoutil/data/models/operations/PowerupOperation.java
@@ -60,24 +60,24 @@ public class PowerupOperation extends Operation {
         final int candies = pokemon.getCandies();
         final int candiesToPowerup = pokemon.getCandyCostsForPowerup();
         if (candies < candiesToPowerup) {
-            return new BpmOperationResult(String.format(
-                    "Insufficent candies, needed %d but had %d ",
-                    candiesToPowerup,
-                    candies),
-                    OperationError.INSUFFICENT_CANDIES);
+            return needSomeToPowerUp(candies, candiesToPowerup,OperationError.INSUFFICENT_CANDIES);
         }
 
         final int stardust = AccountManager.getInstance().getPlayerAccount().getStardust();
         final int stardustToPowerUp = pokemon.getStardustCostsForPowerup();
 
         if (stardust < stardustToPowerUp) {
-            return new BpmOperationResult(String.format(
-                    "Insufficent stardust, needed %d but had %d ",
-                    stardustToPowerUp,
-                    stardust),
-                    OperationError.INSUFFICENT_STARDUSTS);
+            return needSomeToPowerUp(candies, candiesToPowerup,OperationError.INSUFFICENT_STARDUSTS);
         }
 
         return new BpmOperationResult();
+    }
+
+    private BpmOperationResult needSomeToPowerUp(final int exist, final int needToPowerUp, OperationError operationError) {
+        return new BpmOperationResult(String.format(
+                "Insufficent candies, needed %d but had %d ",
+                exist,
+                needToPowerUp),
+                operationError);
     }
 }
